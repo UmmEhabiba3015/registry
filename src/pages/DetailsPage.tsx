35596babb9f6ab6824a1096791fca6d6d7ac9779
Umm E Habiba Kausar <habibaqasim0311@gmail.com>
2024-03-29 06:34:36 +0500

    feat : add basic components and architecture of app

---
+++ src/pages/DetailsPage.tsx
@@ -0,0 +1,5 @@
+export default function DetailsPage () {
+    return (
+        <div>No details for you my friend</div>
+    )
+}
---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,5 @@
+export default function SearchPage () {
+    return (
+        <div>Searching something??</div>
+    )
+}
